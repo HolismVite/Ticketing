0d103ebc710e5061935b189095cfdf3a0e60e4f0
Saeed Nemati <sn.1361@gmail.com>
2022-06-14 08:45:53 +0430

    committed by script

--- Admin/Ticket/Create.jsx
+++ Admin/Ticket/Create.jsx
@@ -11,21 +11,24 @@ const fields =
             display={user => user.displayName}
             choose={user => user.guid}
         />
+        {/* <UserBrowser
+            required
+        /> */}
         <Text
             column='title'
             placeholder='Title'
-            required='Please write the title.'
+            required='Please write the title'
         />
         <Enum
             column='priorityId'
-            entityType='ticketingPriority'
+            entityType='TicketPriority'
             placeholder='Priority'
-            required='Please choose the priority for this ticket. Choose less importance if it is less urgent.'
+            required='Please choose the priority for this ticket. Choose less importance if it is less urgent'
         />
         <LongText
             column='body'
-            placeholder='Please describe the problem!'
-            required='We need to know the problem to be able to help.'
+            placeholder='Please describe the problem'
+            required='We need to know the problem to be able to help'
         />
     </>
 
--- User/Create.jsx
+++ User/Create.jsx
@@ -5,18 +5,18 @@ const fields =
         <Text
             column='title'
             placeholder='Title'
-            required='Please write the title.'
+            required='Please write the title'
         />
         <Enum
             column='TicketPriorityId'
             entityType='TicketPriority'
             placeholder='Priority'
-            required='Please choose the priority for this ticket. Choose less importance if it is less urgent.'
+            required='Please choose the priority for this ticket. Choose less importance if it is less urgent'
         />
         <LongText
             column='body'
-            placeholder='Please describe the problem!'
-            required='We need to know the problem to be able to help.'
+            placeholder='Please describe the problem'
+            required='We need to know the problem to be able to help'
         />
     </>
 
