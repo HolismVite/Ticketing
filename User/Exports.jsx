71ba6c17f0bc9983cdbd10d13684a3f347f31a78
Saeed Nemati <sn.1361@gmail.com>
2022-06-14 08:12:44 +0430

    committed by script

---
+++ User/Exports.jsx
@@ -0,0 +1,25 @@
+import HelpIcon from '@mui/icons-material/Help';
+import Tickets from './List'
+import ViewTicket from './View'
+
+const TicketingRoutes = [
+    {
+        "path": "/tickets",
+        "component": Tickets
+    },
+    {
+        "path": "/ticket/view",
+        "component": ViewTicket
+    },
+]
+
+const TicketingMenu = [
+    {
+        "title": "Tickets",
+        "icon": HelpIcon,
+        "url": "/tickets"
+    }
+]
+
+export { TicketingMenu }
+export { TicketingRoutes }
